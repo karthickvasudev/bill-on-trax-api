280e558d8c25f005c9751f3b2609ff80cf0d7331
karthick <karthick.vasudevan.143@gmail.com>
2025-02-23 14:59:07 +0530

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/billixapp/BillixAppApplication.java
@@ -0,0 +1,13 @@
+package com.billixapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillixAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillixAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/billixapp/BillixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billixapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillixAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
